8c5071632d6abb63daec4cb599f4c5e3425f0c94
sa1if3 <saifur.rahman18@gmail.com>
2019-08-13 19:26:20 +0530

    Commit with footer changes

--- login.php
+++ login.php
@@ -62,7 +62,11 @@
         </div>
         <div class="col-lg-12" style="text-align: center">
         Copyright &COPY; All Right Reserved.<br/>
+
      <a href="https://in.linkedin.com/in/mdsaifur" target="_blank">Md. Saifur Rahman</a>
+
+    By <a href="https://in.linkedin.com/in/mdsaifur" target="_blank">Md. Saifur Rahman</a>
+
     </div>
         
         <!-- /.col -->
--- nav/footer.php
+++ nav/footer.php
@@ -2,6 +2,10 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 1.0.2
     </div>
+
     <strong>Copyright &copy; <a href="https://in.linkedin.com/in/mdsaifur">Md. Saifur Rahman</a></strong> All rights
+
+    <strong>Copyright &copy; <a href="https://in.linkedin.com/in/mdsaifur" target="_blank">Md. Saifur Rahman</a></strong> All rights
+
     reserved.
   </footer>
