b709c884b55860f597a5c2f18a5de206d562eb55
sa1if3 <saifur.rahman18@gmail.com>
2019-08-13 19:26:20 +0530

    Commit with footer changes

--- nav/footer.php
+++ nav/footer.php
@@ -2,6 +2,6 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 1.0.2
     </div>
-    <strong>Copyright &copy; <a href="https://techmion.com">Techmion Solutions India Pvt. Ltd.</a></strong> All rights
+    <strong>Copyright &copy; <a href="https://in.linkedin.com/in/mdsaifur" target="_blank">Md. Saifur Rahman</a></strong> All rights
     reserved.
   </footer>
